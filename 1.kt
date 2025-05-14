968c120d5f1c8a1cf73453b3ee6af7ba9068efc2
DanielBO07 <150436759+DanielBO07@users.noreply.github.com>
2025-05-14 11:27:40 -0300

    1

---
+++ 1.kt
@@ -0,0 +1,16 @@
+fun main() {
+    // escreva a espressão em minusculo e coloque os dosi valores, exemplo subtração(4.0,6,0)
+    divisão(3.0,7.0)
+}
+fun soma(x:Double, y:Double){
+   println("x + y = " + (x + y))
+}
+fun subtração(x:Double, y:Double){
+   println("x - y = " + (x - y))
+}
+fun multiplicação(x:Double, y:Double){
+   println("x * y = " + (x * y))
+}
+fun divisão(x:Double, y:Double){
+   println("x / y = " + (x / y))
+}
---
+++ Graus.kt
@@ -0,0 +1,4 @@
+fun main() {
+    val celsio: Double = 100.0
+    println("$celsio graus célsios é igual a "+(((celsio * 9) / 5) + 32)+" Fahrenheit")
+}
